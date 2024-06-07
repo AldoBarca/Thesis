40d2f557d73d699fbb96faf5ef109b0b474742c0
AldoBarca <81430042+AldoBarca@users.noreply.github.com>
2024-06-07 15:21:24 +0200

    Update creazione_dati_training.py

--- Codice_Tesi/creazione_dati_training.py
+++ Codice_Tesi/creazione_dati_training.py
@@ -129,17 +129,76 @@ def conta_eventi_set(set):
 
     return eventi_classi
 
+
+def return_max_num_eventi(set):
+    eventi=conta_eventi_set(set)
+    i=0
+    for classe,num_eventi in eventi.items():
+        if eventi[classe]>i:
+            i=eventi[classe]
+    return i
+
 print(conta_eventi_set(support_set))
 print(conta_eventi_set(query_set))
+print(return_max_num_eventi(support_set))
+print(return_max_num_eventi(query_set))
 
+#support_vector:[Classe,Evento,]
+#query_label=num_frames
+
+
+def ritorna_numero_frame_minime(set,durata_frame_in_secondi,frame_length,hop_length):
+    num_frames=0
+    for classe,file in set.items():
+        for audio,events in file.items():
+             for evento in events:
+                intervallo=evento['end']-evento['start']
+                sovrapposizione=hop_length/frame_length #quanto effettivamente le frame si sovrappongono l'un l'altra
+                num_frames_evento=intervallo/(durata_frame_in_secondi*sovrapposizione)              #durata di 0.01 se frame=10ms.
+                
+                if (num_frames==0):
+                    num_frames=num_frames_evento
+                elif (num_frames>num_frames_evento):
+                    num_frames=num_frames_evento
+    return num_frames
+numero_frames_support=ritorna_numero_frame_minime(support_set,0.01,1,1)
+numero_frames_query=ritorna_numero_frame_minime(query_set,0.01,1,1)
+"""
+
+Si hanno due soluzioni per il support set:
+1.Considero il massimo di eventi del support set tra i vari audio, ergo 25. 
+A)Devo però riempire di eventi Negativi anche il support set.Concetto che non sono sicuro abbia senso per un support set, che dovrebbe mostrarci 
+solo eventi positivi per definizione.
+B)In alternativa effettuare data augmentation che partendo da un certo numero di campioni arrivi a 25. 
+
+2.Considero il minimo (5) eventi. 5 eventi sarebbe interessante anche perche alleneremmo il modello a lavorare con un support set di k=5 esempi come
+durante l'evaluation, ma dovremmo non usare di fatto molti audio, aspetto che potrebbe essere controproducente in fase di training.
+"""
+
+def create_support_set_training1B(set):
+    classi_set=conta_classi_set(set)
+    max_numero_eventi=return_max_num_eventi(set)
+    num_min_frame=ritorna_numero_frame_minime(support_set)
+    support_set_train=torch.empty(classi_set,max_numero_eventi,num_min_frame,220)
+
+    for classe,file in support_set.items():
+        id_evento=0
+
+        for audio,events in file.items():
+            audio_path=os.path.join(args.traindir,audio)
+            audio_loaded, sr=librosa.load(audio_path)
+            for evento in events:
+                start_campione=int(evento['start']*sr)
+                end_campione=int(evento['end']*sr)
+                audio_evento=audio_loaded[start_campione:end_campione]
 
 
 def create_support_set_training():
    
-    support_set_train=torch.empty(conta_classi_set(support_set),3,3,3)
-    print(support_set_train.shape)
+    support_set_train=torch.empty(conta_classi_set(support_set),300,300,300)
+   
     for classe,file in support_set.items():
-        campioni_positivi_classe=torch.empty(3,3)#[]
+        campioni_positivi_classe=torch.empty(3,3)
         for audio,events in file.items():
             audio_path=os.path.join(args.traindir,audio)
             audio_loaded, sr=librosa.load(audio_path)
@@ -148,14 +207,14 @@ def create_support_set_training():
                     end_campione=int(evento['end']*sr)
                     
                     audio_evento=audio_loaded[start_campione:end_campione]  #frammento di audio contenente solo l'evento positivo
-                    lunghezza_frame=int(0.01*sr)
+                    lunghezza_frame_campioni=int(0.01*sr) #220 campioni per frame
 
                     #scomposizione in frames del frammento di audio dell'evento positivo, diventa un tensore di tipo [180][220]
                     #il 180 varia in base alla lunghezza del frammento audio 
-                    frames=librosa.util.frame(audio_evento, frame_length=lunghezza_frame,hop_length=lunghezza_frame).T    #l'hop lenght lo faccio intanto senza sovrapposizioni, poi vediamo
+                    frames=librosa.util.frame(audio_evento, frame_length=lunghezza_frame_campioni,hop_length=lunghezza_frame_campioni).T    #l'hop lenght lo faccio intanto senza sovrapposizioni, poi vediamo
                     #campioni_positivi_classe.append(frames)
-
-                    torch.cat(campioni_positivi_classe,frames)
+                    frames=torch.from_numpy(frames)
+                    torch.stack(campioni_positivi_classe,frames)
                     
                     
                     
